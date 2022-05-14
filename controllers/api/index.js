91f34f9386958571274c730592e9a27f122afab1
emilyk <emily.g.kruse@gmail.com>
2022-05-14 14:43:06 -0400

    add bootcamp routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
+const bootcampRoutes = require("./bootcamp-routes.js");
 
 router.use("/users", userRoutes);
+router.use("/bootcamps", bootcampRoutes);
 
 module.exports = router;
