3a66e27c3dd7ad72d5183bb4b9a2e12a5f3c6571
“diabm1855”  <motasem.diab@gmail.com>
2022-05-15 16:59:55 -0400

    trying to configure handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
-const bootcampRoutes = require("./bootcamp-routes.js");
+// const bootcampRoutes = require("./bootcamp-routes.js");
 
 router.use("/users", userRoutes);
-router.use("/bootcamps", bootcampRoutes);
+// router.use("/bootcamps", bootcampRoutes);
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require("./config/connection");
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 // const helpers = require('./utils/helpers');
 
@@ -32,8 +32,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   // app.listen(PORT, () => console.log('Now listening'));
+// });
+
+app.listen(PORT, () => console.log('Now listening'));
